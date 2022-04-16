97241ea31d07c3381177c754018a8858dbc1c2c0
NachoMcFly <ignacio.moraga.pineda@ciisa.cl>
2022-04-16 13:53:25 -0400

    trabajo4

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS game (
+CREATE TABLE games (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	gameName VARCHAR(255) NOT NULL,
 	platform VARCHAR(255) NOT NULL,
@@ -6,6 +6,6 @@ CREATE TABLE IF NOT EXISTS game (
 	price VARCHAR(255) NOT NULL
 );
 
-INSERT INTO game (gameName, platform, developer, price) VALUES (
+INSERT INTO games (gameName, platform, developer, price) VALUES (
 	"seis", "dos", "tres", "cuadro"
 );
