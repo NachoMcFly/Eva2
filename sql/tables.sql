8584ba51709a2e834248552b617138554ef67282
NachoMcFly <ignacio.moraga.pineda@ciisa.cl>
2022-04-17 14:30:11 -0400

    atalaya4

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE games (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	gameName VARCHAR(255) NOT NULL,
-	platform VARCHAR(255) NOT NULL,
-	developer VARCHAR(255) NOT NULL,
-	price VARCHAR(255) NOT NULL
+	gameName VARCHAR(50) NOT NULL,
+	platform VARCHAR(50) NOT NULL,
+	developer VARCHAR(50) NOT NULL,
+	price VARCHAR(50) NOT NULL
 );
 
 INSERT INTO games (gameName, platform, developer, price) VALUES (
